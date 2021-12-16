25c705fea13aaca370ed34d7d88d7ab891884789
Jonatas Melo <jonnmello@hotmail.com>
2021-12-16 15:52:12 -0300

    Lesson, Content, Task

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -0,0 +1,115 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)//por que ele vai ter herança,quando voce vai implementar uma herança no banco de dados relacional, os tipos diferentes como uma tabela so quando vc tiver um tipo e outro nao, vc coloca nullo(JOINED)sao as 3 tabelas diferentes
+public abstract class Lesson implements Serializable { // esse abstract nao pode ser instanciada, por que ou vai ser "Content" ou "Task" (herança total)
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Integer position;
+	
+	@ManyToOne 
+	@JoinColumn(name = "section_id")//mapeando chave estrangeira
+	private Section section; //para associação
+	
+	@ManyToMany
+	@JoinTable(name = "tb_lessons_done",
+		joinColumns = @JoinColumn(name = "lesson_id"),
+		inverseJoinColumns = {
+				@JoinColumn(name = "user_id"),  //criando uma tabela de associação com 3 colunas
+				@JoinColumn(name = "offer_id")
+		}
+	)
+	private Set<Enrollment> enrollmentsDone = new HashSet<>(); //muitos pra muitos usa Set
+	// nao coloca a construção set no construtor (logo abaixo) tb não tem metodo set só get
+	public Lesson() {
+		
+	}
+
+	public Lesson(Long id, String title, Integer position, Section section) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.position = position;
+		this.section = section;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	public Section getSection() {
+		return section;
+	}
+
+	public void setSection(Section section) {
+		this.section = section;
+	}
+
+	public Set<Enrollment> getEnrollmentsDone() {
+		return enrollmentsDone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+
+	
+	
+}
