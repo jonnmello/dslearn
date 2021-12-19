5469797212609f3a99883e7ecd5ef909238a1396
Jonatas Melo <jonnmello@hotmail.com>
2021-12-19 11:11:38 -0300

    Preauthorizing methods

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearnbds.entities.Deliver;
 
-public interface DeliveryRepository extends JpaRepository<Deliver, Long>{
+public interface DeliverRepository extends JpaRepository<Deliver, Long>{
 
 }
 
